40ecb0fb073ac704224cba376363862b3a646219
Matt Patterson <matt@reprocessed.org>
2017-03-14 15:46:24 +0100

    Add a very basic Point object
    No idea about units, but has x and y

---
+++ spec/box_dividers/point_spec.rb
@@ -0,0 +1,15 @@
+require 'box_dividers/point'
+
+module BoxDividers
+  RSpec.describe Point do
+    subject { Point.new(1, 2) }
+
+    it "returns its x" do
+      expect(subject.x).to eq(1)
+    end
+
+    it "returns its y" do
+      expect(subject.y).to eq(2)
+    end
+  end
+end
--- spec/box_dividers_spec.rb
+++ spec/box_dividers_spec.rb
@@ -1,11 +1,5 @@
-require "spec_helper"
-
 RSpec.describe BoxDividers do
   it "has a version number" do
     expect(BoxDividers::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
