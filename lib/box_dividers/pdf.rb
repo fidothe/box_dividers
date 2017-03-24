4aee1901e96bec09633763090133ede93b33c990
Matt Patterson <matt@reprocessed.org>
2017-03-24 17:18:31 +0100

    Use new BoundingBox to build top+bottom PDFs

--- lib/box_dividers/pdf.rb
+++ lib/box_dividers/pdf.rb
@@ -1,16 +1,17 @@
+require_relative 'transformations'
 require 'prawn'
 
 module BoxDividers
   class Pdf
     include Prawn::View
 
-    attr_reader :path
+    attr_reader :bounding_box
 
-    def initialize(path)
-      @path = path
+    def initialize(bounding_box)
+      @bounding_box = bounding_box
     end
 
-    def draw_divider
+    def draw_path(path)
       points = path.points.dup
       first_point = points.shift
       close_and_stroke do
@@ -19,6 +20,12 @@ module BoxDividers
           line_to point.x, point.y
         end
         line_to first_point.x, first_point.y
+      end
+    end
+
+    def draw_divider
+      bounding_box.transform(Transformations.mm_to_pt).paths.each do |path|
+        draw_path(path)
       end
     end
   end
